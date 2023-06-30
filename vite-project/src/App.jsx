3b216e654390118ef57bf190fde47bd6ce3f407e
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-07-01 01:10:45 +0330

    Add: add some codes to App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,38 @@
 import './App.css'
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState('');
+    const [names, setNames] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchHandler = (e) => {
+        setSearchTerm(e.target.value);
+    }
+    const inputChangeHandler = (e) => {
+        setValue(e.target.value);
+    }
+    const submitFormHandler = (e) => {
+        e.preventDefault();
+        if (value.trim()) {
+            setNames([...names, {id: names.length + 1, name: value}]);
+            setValue('');
+        } else {
+            alert("Please enter a name");
+        }
+    }
 
     return (
-        <div className="container">
-            <div className="search">
-                <input className="search-input" placeholder='Search...'/>
-            </div>
-            <div className="input">
-                <input id="input-container" placeholder="Name" type="text"/>
-                <input id="input-container" placeholder="Phone" type="number"/>
-                <input id="input-container" placeholder="Email" type="email"/>
-                <button className="button">+Add</button>
-            </div>
+        <div>
+            <h1 className="header">Contact List</h1>
+            <form className="search">
+                <input className="search-input" placeholder='Search...' value={searchTerm} onChange={searchHandler}/>
+            </form>
+            <form className="input" onSubmit={submitFormHandler}>
+                <input id="input-container" placeholder="Name" value={value} onChange={inputChangeHandler}/>
+                <input id="input-container" placeholder="Phone"/>
+                <input id="input-container" placeholder="Email"/>
+                <button className="button" type='submit'>+Add</button>
+            </form>
             <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70}}/>
             <div className="titles">
                 <span className="titles-container">
@@ -27,6 +46,11 @@ function App() {
                 </span>
             </div>
             <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70}}/>
+            <ul>
+                {names
+                    .filter(({name}) => name.includes(searchTerm))
+                    .map(({id, name}) => <li key={id}>{name}</li>)}
+            </ul>
         </div>
     )
 }
