869766b65c3ec9703a47a9bf353330e87e8c8002
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-07-01 14:13:14 +0330

    Add: final commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import React, {useState} from "react";
 
 function App() {
-    const [names, setNames] = useState([]);
+    const [names, setNames] = useState('');
+    const [numbers, setNumbers] = useState('');
+    const [emails, setEmails] = useState('');
+    const [persons, setPersons] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const [numbers, setNumbers] = useState([]);
-    const [emails, setEmails] = useState([]);
     // const [searchNumbers, setSearchNumbers] = useState('');
     // const [searchEmails, setSearchEmails] = useState('');
     const emailChangeHandler = (e) => {
@@ -15,70 +16,78 @@ function App() {
 
     const numberChangeHandler = (e) => {
         setNumbers(e.target.value);
-    }
-    const searchHandler = (e) => {
-        setSearchTerm(e.target.value);
-        // setSearchNumbers(e.target.value);
-        // setSearchEmails(e.target.value);
+        console.log(e.target.value)
     }
     const nameChangeHandler = (e) => {
         setNames(e.target.value);
         console.log(e.target.value);
     }
+    const searchHandler = (e) => {
+        setSearchTerm(e.target.value);
+    }
     const submitFormHandler = (e) => {
         e.preventDefault();
         if (names.trim() && numbers.trim() && emails.trim()) {
-            setNames([...names, {id: names.length + 1, name: names}]);
-            setNumbers([...numbers, {id: numbers.length + 1, number: numbers}]);
-            setEmails([...emails, {id: emails.length + 1, email: emails}])
-            setNames([]);
-            setNumbers([]);
-            setEmails([]);
+            setPersons([...persons, {id: persons.length + 1, name: names, number: numbers, email: emails}]);
+            setNames('');
+            setNumbers('');
+            setEmails('');
         } else {
             alert("Please type something");
         }
     }
 
     return (
-        <div>
+        <div className="container">
             <h1 className="header">Contact List</h1>
-            <form className="input search" onSubmit={submitFormHandler}>
-                <input className="search-input" placeholder='Search...' value={searchTerm} onChange={searchHandler}/>
-                <input id="input-container" placeholder="Name" value={names} onChange={nameChangeHandler}/>
-                <input id="input-container" placeholder="Phone" value={numbers} onChange={numberChangeHandler}/>
-                <input id="input-container" placeholder="Email" value={emails} onChange={emailChangeHandler}/>
-                <button className="button" type='submit'>+Add</button>
+            <form onSubmit={submitFormHandler}>
+                <div className="search">
+                    <input className="search-input" placeholder='Search...' value={searchTerm}
+                           onChange={searchHandler}/>
+                </div>
+                <div className="input">
+                    <input id="input-container" placeholder="Name" value={names} onChange={nameChangeHandler}/>
+                    <input id="input-container" placeholder="Phone" value={numbers} onChange={numberChangeHandler}/>
+                    <input id="input-container" placeholder="Email" value={emails} onChange={emailChangeHandler}/>
+                    <button className="button" type='submit'>+Add</button>
+                </div>
             </form>
             <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70}}/>
             <div className="titles">
-                <span className="titles-container">
-                    <img src="../public/icons8-name-40.png" alt="name-icon"/>Name
+                    <span className="titles-container">
+                        Name<img src="../public/icons8-name-40.png" alt="name-icon"/>
                     </span>
                 <span className="titles-container">
-                    <img src="../public/icons8-phone-40.png" alt="phone-icon"/>Phone
-                </span>
+                        Phone<img src="../public/icons8-phone-40.png" alt="phone-icon"/>
+                    </span>
                 <span className="titles-container">
-                    <img src="../public/icons8-email-40.png" alt="email-icon"/>Email
-                </span>
+                        Email<img src="../public/icons8-email-40.png" alt="email-icon"/>
+                    </span>
             </div>
-            <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70}}/>
-            <ul>
-                {names
-                    .filter(({name}) => name.includes(searchTerm))
-                    .map(({id, name}) => {return(<li key={id}>{name}</li>)})}
-            </ul>
-            <ul>
-                {numbers
-                    .filter(({number}) => number.includes(searchNumbers))
-                    .map(({id, number}) => <li key={id}>{number}</li>)
-                }
-            </ul>
-            <ul>
-                {emails
-                    .filter(({email}) => email.includes(searchEmails))
-                    .map(({id, email}) => <li key={id}>{email}</li>)
-                }
-            </ul>
+            <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70,marginBottom:2}}/>
+            <div>
+                {persons
+                    .filter(
+                        ({name, number, email}) =>
+                            name.includes(searchTerm) ||
+                            number.includes(searchTerm) ||
+                            email.includes(searchTerm)
+                    )
+                    .map(({id, name, number, email}) => (
+                        <div className="list-container">
+                            <div className="list-item" key={id}>
+                                {name}
+                            </div>
+                                <div className="list-item" key={id}>
+                                    {number}
+                                </div>
+                                <div className="list-item" key={id}>
+                                    {email}
+                                </div>
+                        </div>
+                    ))}
+                <hr style={{borderColor: "#B6DCFE", marginLeft: 70, marginRight: 70, marginBottom:2}}/>
+            </div>
         </div>
     )
 }
